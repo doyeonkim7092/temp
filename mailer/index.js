64e2af26dc6288db911bc03d9974842121c2b9d8
doyeonkim7092 <gothone7092@gmail.com>
2021-01-11 20:54:12 +0900

    ADD mailing TEST && .env SETTING

---
+++ mailer/index.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv").config();
+const myPw = process.env.PASSWORD;
+
+// email message options
+
+const mailOptions = {
+  from: "gothon7092@gmail.com",
+  to: "gothone@naver.com",
+  subject: "Email from Node_App",
+  text: "test for nodemailer",
+};
+
+// email transport configuration
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "gothone7092@gmail.com",
+    pass: myPw,
+  },
+});
+
+// send Email
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`E-mail SENDED ~ ${info.response}`);
+  }
+});
